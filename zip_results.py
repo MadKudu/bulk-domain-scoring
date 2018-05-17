335695f706eef356ce4e6c970351f11ea8a6c17d
Thomas Wilgenbus <thomas.wilgenbus@gmail.com>
2018-05-17 15:05:40 +0200

    Fix inconsistency when data's partially faulty

--- zip_results.py
+++ zip_results.py
@@ -11,17 +11,24 @@ def zip_results(filename: str):
     workbook = load_workbook(filename=filename, keep_vba=False)
     sheet = workbook.active
 
+    skiped = 0
     result_filename = filename.replace(".xlsx", ".csv")
     print("File loaded. results/{} will be zipped to it.".format(result_filename))
-    with open("results/" + result_filename, "r") as result:
+    with open("results/" + result_filename, "rb") as result:
         result.seek(0)
         rows = sheet.max_row
         columns = sheet.max_column
         for line in range(3, rows):
             if line % 100 == 0:
                 print("Zipped {0:.3f}%".format(line / (rows * 1.) * 100.))
-            domain, segment, score = result.readline().split(",")
+            result_line = result.readline()
+            if not result_line:
+                # xlsx had some invalid lines, some where skiped
+                # resulting a discrepancy between the two files lines numbers
+                break
+            domain, segment, score = result_line.decode("utf-8").split(",")
             if not sheet.cell(line, 6).value or not regex.search(sheet.cell(line, 6).value):
+                result.seek(-len(result_line), 1)  # go back to read the same line again
                 continue  # skip invalid entries, like in bulk_score
             if domain not in sheet.cell(line, 6).value:
                 print("ERROR! Zipping on weird data (trying to zip {} with {})".format(sheet.cell(line, 6).value,
