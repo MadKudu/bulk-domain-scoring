9be0a48c53ab2b48fa9fa962927bb3e89e283fa6
Paul Cothenet <paul@madkudu.com>
2019-10-06 16:56:09 -0700

    Comment section

--- bulk_score.py
+++ bulk_score.py
@@ -65,29 +65,45 @@ def run_xls(filename: str, api_key: str, score_type: str, column_idx: int):
 
                     if domain not in domains_scored:
                         params = {"domain": domain}
-
+                        print(params)
                         resp = requests.get(API_DOMAIN_URL, auth=(api_key, ''), params=params)
-                        print(resp.json())
-                        domains_scored[domain] = resp.json()['properties']['customer_fit']
+                        customer_fit_result = resp.json()['properties']['customer_fit']
+                        print(customer_fit_result)
+                        domains_scored[domain] = customer_fit_result
 
                     customer_fit = domains_scored[domain]
-                    result.write(
-                        "{},{},{}\n".format(
-                            domain, customer_fit['segment'], customer_fit['score'], customer_fit['top_signals_formatted'])
-                    )
+                    new_row = "{},{},{},{}\n".format(domain, customer_fit['segment'], customer_fit['score'], customer_fit['top_signals_formatted'])
+                    print(new_row)
+                    result.write(new_row)
+
+                # if score_type == 'domain':
+                #     domain = person["email"]
+
+                #     if domain not in domains_scored:
+                #         params = {"email": "user@{}".format(domain)}
+                #         print(params)
+                #         resp = requests.get(API_PERSON_URL, auth=(api_key, ''), params=params)
+                #         customer_fit_result = resp.json()['properties']['customer_fit']
+                #         print(customer_fit_result)
+                #         domains_scored[domain] = customer_fit_result
+
+                #     customer_fit = domains_scored[domain]
+                #     new_row = "{},{},{},{}\n".format(domain, customer_fit['segment'], customer_fit['score'], customer_fit['top_signals_formatted'])
+                #     print(new_row)
+                #     result.write(new_row)
+
                 if score_type == 'email':
                     email = person["email"]
                     if email not in emails_scored:
                         params = {"email": email}
-
                         resp = requests.get(API_PERSON_URL, auth=(api_key, ''), params=params)
-                        customer_fit = resp.json()['properties']['customer_fit']
-                        emails_scored[email] = resp.json()['properties']['customer_fit']   
-                    customer_fit = emails_scored[email]                     
-                    result.write(
-                        "{},{},{},{}\n".format(
-                            email, customer_fit['segment'], customer_fit['score'], customer_fit['top_signals_formatted'])
-                    )        
+                        customer_fit_result = resp.json()['properties']['customer_fit']
+                        emails_scored[email] = customer_fit_result
+
+                    customer_fit = emails_scored[email]      
+                    new_row = "{},{},{},{}\n".format(domain, customer_fit['segment'], customer_fit['score'], customer_fit['top_signals_formatted'])
+                    print(new_row)
+                    result.write(new_row)        
         except Exception:
             result.flush()
             logger.exception("Exception met. Relaunch to resume!\n")
